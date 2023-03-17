6cfa1a638074c6aefd545e19e9753e133ba9b539
Ignacio Morinigo <morinacho@gmail.com>
2023-03-17 14:09:15 -0300

    Nav menu css

--- js/function.js
+++ js/function.js
@@ -1,1 +1,6 @@
- 
+let nav = document.querySelector('#header-navbar'),
+    navPosition = nav.offsetTop
+    navTop = nav.style.top;
+    window.onscroll = () =>{
+    (window.pageYOffset >= navPosition) ? nav.style.color = 'black': nav.style.color = navTop;
+};
