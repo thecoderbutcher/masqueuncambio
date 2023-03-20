077ad001d4b61f1cc1bd9c39659fb417d9c80806
Ignacio Morinigo <morinacho@gmail.com>
2023-03-20 09:55:50 -0300

    Testimonios

--- js/function.js
+++
@@ -1,6 +0,0 @@
-let nav = document.querySelector('#header-navbar'),
-    navPosition = nav.offsetTop
-    navTop = nav.style.top;
-    window.onscroll = () =>{
-    (window.pageYOffset >= navPosition) ? nav.style.color = 'black': nav.style.color = navTop;
-};
